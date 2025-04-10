317cf607d65d120321150c0df1d504119b5f5e72
YuuhiXD <kusek-kuba@o2.pl>
2025-04-10 23:39:00 +0200

    lab 5 zad 8

--- genealogy/src/Person.java
+++ genealogy/src/Person.java
@@ -1,12 +1,50 @@
 import java.time.LocalDate;
+import java.util.*;
 
-public class Person {
-    private String firstName, lastName;
-    private LocalDate birthday;
+import static java.util.List.copyOf;
 
-    public Person(String firstName, String lastName, LocalDate birthday) {
+public class Person implements Comparable<Person> {
+    private String firstName,lastName;
+    private LocalDate birthDay;
+    private SortedSet<Person> children;
+
+    public Person(String firstName, String lastName, LocalDate birthDay) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.birthday = birthday;
+        this.birthDay = birthDay;
+        this.children = new TreeSet<>();
+    }
+    public boolean adopt(Person child)
+    {
+        return this.children.add(child);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthDay=" + birthDay +
+                ", children=" + children +
+                '}';
+    }
+
+    public Person getYoungestChild(){
+        if(children.isEmpty()) return null;
+
+        return children.last();
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return this.birthDay.compareTo(o.birthDay);
+    }
+
+    public List<Person> getChildren(){
+        return List.copyOf(children);
+    }
+
+    public String name(){
+        return this.firstName + " " + this.lastName;
     }
 }
