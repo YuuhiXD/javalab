823d07b96e8a3355c5e85bef50cd26f72b2b0382
YuuhiXD <kusek-kuba@o2.pl>
2025-04-11 08:50:39 +0200

    lab 6 zad 1

---
+++ genealogy/src/Main.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = Person.fromCsvLine("Marek Kowalski,15.05.1899,25.06.1957,,");
+        System.out.println(person);
+
+    }
+}
